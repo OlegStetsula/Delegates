1bfc828d6a4b7d0259882ec9c2dc837d50ca6c7f
OlegStetsula <olegstetsula@gmail.com>
2019-07-07 14:49:03 +0300

    Add project files.

---
+++ Delegates/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Delegates
+{
+    public static class Operations
+    {
+        public delegate bool Operation(float a);
+
+        public static float Sum(float[] arr, Operation op)
+        {
+            float sum = arr.Where(n => op(n)).Sum();
+            return sum;
+        }
+
+        public static float Max(float[] arr, Operation op)
+        {
+            float max = arr.Where(n => op(n)).Max();
+            return max;
+        }
+
+        public static float Avg(float[] arr, Operation op)
+        {
+            float avg = arr.Where(n => op(n)).Average();
+            return avg;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations.Operation op = x => x > 0;
+            float[] arr = { -4, 0, 4, 3, -9, 8, 2};
+            float sum = Operations.Sum(arr, op);
+
+            Console.WriteLine("sum of array is {0}", sum);
+
+            float max = Operations.Max(arr, op);
+            Console.WriteLine("max of array is {0}", max);
+
+            float avg = Operations.Avg(arr, op);
+            Console.WriteLine("avg of array is {0}", avg);
+
+            Console.ReadLine();
+
+        }
+    }
+}
